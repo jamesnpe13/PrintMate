40b99f655563d8e16346807e6652bffaf6e05242
James Elazegui <jameselazegui21@gmail.com>
2024-11-28 11:45:56 +1300

    Squashed commit of the following:
    commit 2c66875399d23dd2f9037babb3bbbdd8903a1e73
    Author: James Elazegui <jameselazegui21@gmail.com>
    Date:   Thu Nov 28 11:42:59 2024 +1300
    
        ServiceCollection
    
    commit 365e1e69f2418308bef360793f4c51b67be0c98d
    Author: James Elazegui <110789359+jamesnpe13@users.noreply.github.com>
    Date:   Sat Nov 23 22:05:06 2024 +1300
    
        Update README.md
    
    commit d9fc789bb987c4de182ea22e574b989e7c5c3c79
    Author: James Elazegui <jameselazegui21@gmail.com>
    Date:   Sat Nov 23 21:54:41 2024 +1300
    
        Nuget packages installed
    
    commit f2966e18175b12d566fffd1157f446c3d7a14a69
    Author: James Elazegui <jameselazegui21@gmail.com>
    Date:   Sat Nov 23 21:39:26 2024 +1300
    
        Directories
    
    commit 87f4495a819840bacc14aaf9aaba6bd6be0290ee
    Author: James Elazegui <jameselazegui21@gmail.com>
    Date:   Sat Nov 23 21:35:57 2024 +1300
    
        Init

---
+++ PrintMate/PrintMate/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace PrintMate;
+
+public partial class App : Application
+{
+    // enable console for debuggin
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool AllocConsole();
+
+    public IServiceProvider ServiceProvider { get; private set; }
+
+    public App()
+    {
+        InitializeComponent();
+        AllocConsole(); // enables console
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ServiceCollection serviceCollection = new();
+        ConfigureServices(serviceCollection);
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
+}
